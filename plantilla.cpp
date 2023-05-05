0f61072adb9ecbef2509352dc04506f26989eedd
LibardoNavarro <josenavarrop05@gmail.com>
2023-05-05 13:23:50 -0500

    Arreglando cosas

--- plantilla.cpp
+++ plantilla.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
 typedef vector<int> vi;
 typedef vector<long long> vl;
+#define INF 1000000000;
 
 int main() {
     freopen("file.in", "r", stdin);
