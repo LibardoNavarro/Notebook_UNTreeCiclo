82cee2606eb37b5f07895935d79fed200c375cc0
LibardoNavarro <josenavarrop05@gmail.com>
2023-06-26 22:46:51 -0500

    Segment tree

---
+++ code/Segment tree/Dynamic_Range_Sum_Queries.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(arr) ((int) arr.size())
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+const int INF = 1e9;
+const ll INFL = 1e18;
+const int MOD = 1e9+7;
+int dirx[4] = {0,-1,1,0};
+int diry[4] = {-1,0,0,1};
+int dr[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+int nullValue = 0;
+
+struct nodeST{
+    nodeST *left, *right;
+    int l, r; ll value;
+
+    nodeST(vi &v, int l, int r) : l(l), r(r){
+        int m = (l+r)>>1;
+        if (l!=r){
+            left = new nodeST(v, l, m);
+            right = new nodeST(v, m+1, r);
+            value = opt(left->value, right->value);
+        }
+        else{
+            value = v[l];
+        }
+    }
+
+    ll opt(ll leftValue, ll rightValue){
+        return leftValue + rightValue;
+    }
+
+    ll get(int i, int j){
+        if (l>=i && r<=j) return value;
+        if (l>j  || r<i) return nullValue;
+
+        return opt(left->get(i, j), right->get(i, j));
+    }
+
+    void upd(int k, int nv){
+        if (l>k  || r<k) return;
+        if (l>=k && r<=k){
+            value = nv;
+            return;
+        }
+
+        left->upd(k, nv);
+        right->upd(k, nv);
+
+        value = opt(left->value, right->value);
+        
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, q; cin >> n >> q;
+    vi v(n);
+
+    for (int i = 0; i<n; i++) cin >> v[i];
+
+    nodeST st(v, 0, n-1);
+
+    for (int i = 0; i<q; i++){
+        int o, a, b; cin >> o >> a >> b;
+
+        if (o == 1) st.upd(a-1, b);
+
+        else if (o == 2) cout << st.get(a-1, b-1) << "\n";
+
+    }
+}
---
+++ code/Segment tree/Range_Update_Queries.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sz(arr) ((int) arr.size())
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+const int INF = 1e9;
+const ll INFL = 1e18;
+const int MOD = 1e9+7;
+int dirx[4] = {0,-1,1,0};
+int diry[4] = {-1,0,0,1};
+int dr[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+int nullValue = 0;
+
+struct nodeST{
+    nodeST *left, *right;
+    int l, r; ll value, lazy;
+
+    nodeST(vi &v, int l, int r) : l(l), r(r){
+        int m = (l+r)>>1;
+        lazy = 0;
+        if (l!=r){
+            left = new nodeST(v, l, m);
+            right = new nodeST(v, m+1, r);
+            value = opt(left->value, right->value);
+        }
+        else{
+            value = v[l];
+        }
+    }
+
+    ll opt(ll leftValue, ll rightValue){
+        return leftValue + rightValue;
+    }
+
+    void propagate(){
+        if (lazy){
+            value += lazy * (r-l+1);
+            if (l!=r){
+                left->lazy += lazy, right->lazy += lazy;
+            }
+            lazy = 0;
+        }
+    }
+
+    ll get(int i, int j){
+        propagate();
+        if (l>=i && r<=j) return value;
+        if (l>j  || r<i) return nullValue;
+
+        return opt(left->get(i, j), right->get(i, j));
+    }
+
+    void upd(int i, int j, int nv){
+        propagate();
+        if (l>j  || r<i) return;
+        if (l>=i && r<=j){
+            lazy += nv;
+            propagate();
+            // value = nv;
+            return;
+        }
+
+        left->upd(i, j, nv);
+        right->upd(i, j, nv);
+
+        value = opt(left->value, right->value);
+        
+    }
+
+    void upd(int k, int nv){
+        if (l>k  || r<k) return;
+        if (l>=k && r<=k){
+            value = nv;
+            return;
+        }
+
+        left->upd(k, nv);
+        right->upd(k, nv);
+
+        value = opt(left->value, right->value);
+        
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, q; cin >> n >> q;
+    vi v(n);
+
+    for (int i = 0; i<n; i++) cin >> v[i];
+
+    nodeST st(v, 0, n-1);
+
+    for (int i = 0; i<q; i++){
+        int o; cin >> o;
+        if (o==1){
+            int a, b, u; cin >> a >> b >> u;
+            st.upd(a-1, b-1, u);
+        }
+        else{
+            int k; cin >> k;
+            cout << st.get(k-1, k-1) << "\n";
+        }
+    }
+}
