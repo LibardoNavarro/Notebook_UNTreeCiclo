673dac79f5663a8648d8a28cba82cce6212bbf34
LibardoNavarro <josenavarrop05@gmail.com>
2023-08-28 17:06:08 -0500

    update notebook

---
+++ code/Bitmask.cpp
@@ -0,0 +1,62 @@
+// Todas son O(1)Representacion
+int a = 5; // Representacion binaria: 0101
+int b = 3; // Representacion binaria: 0011
+// Operaciones Principales
+int resultado_and = a & b; // 0001 (1 en decimal)
+int resultado_or = a | b; // 0111 (7 en decimal)
+int resultado_xor = a ^ b; // 0110 (6 en decimal)
+
+int num = 42; // Representacion binaria: 00101010
+bitset<8> bits(num); // Crear un objeto bitset a partir del numero
+cout << "Secuencia de bits: " << bits << "\n";
+bits.count(); // Cantidad de bits activados
+bits.set(3, true); // Establecer el cuarto bit en 1
+bits.reset(6);     // Establecer el septimo bit en 0
+
+ll S,T;
+// Operaciones con bits (/*) por 2 (redondea de forma automatica)
+S=34; // == 100010
+S = S<<1; // == S*2 == 68 == 1000100
+S = S>>2; // == S/4 == 17 == 10001
+S = S>>1; // == S/2 == 8  == 1000
+
+// Encender un bit
+S = 34;
+S = S|(1<<3); // S = 42 (101010) 
+
+// Limpiar o apagar un bit
+// ~: Not operacion
+S = 42;
+S &= ~(1<<1); // S = 40 (101000)
+
+// Comprobar si un bit esta encendido
+S = 42;
+T = S&(1<<3); // (!= 0): el tercer bit esta encendido
+
+// Invertir el estado de un bit
+S = 40;
+S ^=(1<<2); // 44 (101100)
+
+// LSB (Primero de la derecha)
+S = 40;
+T = ((S) & -(S)); // 8 (001000)
+__builtin_ctz(T); // nos entrega el indice del LSB
+
+// Encender todos los bits
+ll n = 3; // el tamanio del set de bits
+S = 0;
+S = (1<<n) - 1; // 7 (111)
+
+// Enumerar todos los posibles subsets de un bitmask
+int mask = 18;
+for (int subset = mask; subset; subset = (mask & (subset-1))){
+    cout << subset << "\n";
+}
+
+// otras funciones de c++
+__builtin_popcount(32); // 100000 (base 2), only 1 bit is on
+__builtin_popcount(30);// 11110 (base 2), 4 bits are on
+__builtin_popcountl((1l<<62)-1l); // 2^62-1 has 62 bits on (near limit)
+__builtin_ctz(32); // 100000 (base 2), 5 trailing zeroes
+__builtin_ctz(30); // 11110 (base 2), 1 trailing zero
+__builtin_ctzl(1l<<62); // 2^62 has 62 trailing zeroes
