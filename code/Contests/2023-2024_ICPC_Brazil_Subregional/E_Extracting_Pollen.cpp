bf69aecafbc6b55f863b1450dfd93b1aed54be0f
LibardoNavarro <josenavarrop05@gmail.com>
2023-09-23 06:40:47 -0500

    Problem E

---
+++ code/Contests/2023-2024_ICPC_Brazil_Subregional/E_Extracting_Pollen.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(arr) ((int) arr.size())
+#define all(v) v.begin(), v.end()
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+
+const int INF = 1e9;
+const ll INFL = 1e18;
+const int MOD = 1e9+7;
+const double EPS = 1e-9;
+
+int dirx[4] = {0, -1, 1, 0};
+int diry[4] = {-1, 0, 0, 1};
+int dr[] = {1, 1, 0, -1, -1, -1, 0, 1};
+int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+const string ABC = "abcdefghijklmnopqrstuvwxyz";
+const char ln = '\n';
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << setprecision(20) << fixed;
+
+    int n, k; cin >> n >> k;
+
+    vi l(n);
+    int maxi = -INF;
+    for (int i = 0; i<n; i++) {cin >> l[i]; maxi = max(maxi, l[i]);}
+
+    vi v(maxi+1, 0);
+    for (int i : l) v[i]++;
+
+    for (int i = maxi; i>=0; i--){
+        if (v[i]){
+            k-=v[i];
+            int d = 0;
+            int i1 = i;
+            while (i1>0){
+                d += i1%10;
+                i1 /= 10;
+            }
+            v[i-d] += v[i];
+            if (k <= 0) {cout << d << ln; return 0;}
+        }
+    }
+
+    cout << 0 << ln;
+
+    
+    return 0;
+}
