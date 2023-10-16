5cfc6981c07b589746784ba97deb7d5e3ea5a188
LibardoNavarro <josenavarrop05@gmail.com>
2023-10-16 10:07:40 -0500

    Pequeño cambio

--- code/Grafos/Floyd_Warshall.cpp
+++ code/Grafos/Floyd_Warshall.cpp
@@ -2,12 +2,16 @@
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int V; cin >> V;
-    vector<vi> adjMat(V+1, vi(V+1));
+    int n; cin >> n;
+    vector<vi> adjMat(n+1, vi(n+1));
     //Condicion previa: adjMat[i][j] contiene peso de la arista (i, j)
     //o INF si no existe esa arista
-    for (int k = 0; k<V; k++)
-        for (int i = 0; i<V; i++)
-            for (int j = 0; j<V; j++)
-                adjMat[i][j] = min(adjMat[i][j], adjMat[i][k] + adjMat[k][j]);
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (adjMat[i][k] < INF && adjMat[k][j] < INF)
+                    adjMat[i][j] = min(adjMat[i][j], adjMat[i][k] + adjMat[k][j]); 
+            }
+        }
+    }
 }
