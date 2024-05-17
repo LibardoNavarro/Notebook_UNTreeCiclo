8c5b6a23a88125218e99a0679f911af37cc0de70
LibardoNavarro <josenavarrop05@gmail.com>
2024-05-16 19:24:35 -0500

    Pollard Rho

--- code/Matematicas/Miller_Rabin.cpp
+++ code/Matematicas/Miller_Rabin.cpp
@@ -1,45 +1,40 @@
-using u64 = uint64_t;
-using u128 = __uint128_t;
-
-u64 binpower(u64 base, u64 e, u64 mod) {
-    u64 result = 1;
-    base %= mod;
-    while (e) {
-        if (e & 1)
-            result = (u128)result * base % mod;
-        base = (u128)base * base % mod;
-        e >>= 1;
+ll mul (ll a, ll b, ll mod) {
+  ll ret = 0;
+  for(a %= mod, b %= mod; b != 0;
+    b >>= 1, a <<= 1, a = a >= mod ? a - mod : a) {
+    if (b & 1) {
+      ret += a;
+      if (ret >= mod) ret -= mod;
     }
-    return result;
+  }
+  return ret;
 }
-
-bool check_composite(u64 n, u64 a, u64 d, int s) {
-    u64 x = binpower(a, d, n);
-    if (x == 1 || x == n - 1)
-        return false;
-    for (int r = 1; r < s; r++) {
-        x = (u128)x * x % n;
-        if (x == n - 1)
-            return false;
-    }
-    return true;
-};
-
-bool MillerRabin(u64 n, int iter=5) { // returns true if n is probably prime, else returns false.
-    if (n < 4)
-        return n == 2 || n == 3;
-
-    int s = 0;
-    u64 d = n - 1;
-    while ((d & 1) == 0) {
-        d >>= 1;
-        s++;
-    }
-
-    for (int i = 0; i < iter; i++) {
-        int a = 2 + rand() % (n - 3);
-        if (check_composite(n, a, d, s))
-            return false;
-    }
-    return true;
+ll fpow (ll a, ll b, ll mod) {
+  ll ans = 1;
+  for (; b; b >>= 1, a = mul(a, a, mod))
+    if (b & 1)
+      ans = mul(ans, a, mod);
+  return ans;
+}
+bool witness (ll a, ll s, ll d, ll n) {
+  ll x = fpow(a, d, n);
+  if (x == 1 || x == n - 1) return false;
+  for (int i = 0; i < s - 1; i++) {
+    x = mul(x, x, n);
+    if (x == 1) return true;
+    if (x == n - 1) return false;
+  }
+  return true;
+}
+ll test[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 0};
+bool is_prime (ll n) {
+  if (n < 2) return false;
+  if (n == 2) return true;
+  if (n % 2 == 0) return false;
+  ll d = n - 1, s = 0;
+  while (d % 2 == 0) ++s, d /= 2;
+  for (int i = 0; test[i] && test[i] < n; ++i)
+    if (witness(test[i], s, d, n))
+      return false;
+  return true;
 }
