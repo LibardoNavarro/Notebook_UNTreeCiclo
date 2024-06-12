777a6376c282440b938990a682b153ecf8135008
drifterDev <maalvarezmu@unal.edu.co>
2024-06-12 09:11:59 -0500

    Más cambios

--- code/Strings/kmp.cpp
+++ code/Strings/kmp.cpp
@@ -1,33 +1,44 @@
-// Implementar primero prefix_function
-// O(t+p)
-int matches=0;
-void kmp(string &t, string &p){ 
-  vi phi=prefix_function(p);
-  for(int i=0,j=0;i<sz(t);i++){
-    while(j>0 && t[i]!=p[j])j=phi[j-1];
-    if(t[i]==p[j])j++;
-    if(j==sz(p)){
-      cout<<i-j+1<<" "; // Posicion de la ocurrencia
-      matches++;
-      j=phi[j-1];
-    }
-  }
+vi prefix_function(string s){
+	int n=sz(s);
+	vi pi(n);
+	for(int i=1;i<n;i++) {
+		int j=pi[i-1];
+		while(j>0 && s[i]!=s[j])j=pi[j-1];
+		if (s[i]==s[j])j++;
+		pi[i]=j;
+	}
+	return pi;
 }
 
-// Devuelve el arreglo de matches sin implementar prefix_function
+// O(t+p)
+int matches=0;
+void kmp(string& t, string& p){ 
+	vi phi=prefix_function(p);
+	for(int i=0,j=0;i<sz(t);i++){
+		while(j>0 && t[i]!=p[j])j=phi[j-1];
+		if(t[i]==p[j])j++;
+		if(j==sz(p)){
+			cout<<i-j+1<<" "; // Posición de la ocurrencia
+			matches++;
+			j=phi[j-1];
+		}
+	}
+}
+
+// Devuelve el arreglo de matches
 const int MAX=2e5+9;
 int pi[MAX];
-// Pasar el arreglo int d con tamano len(t)
-void kmp_vi(string& p, string& t, int *d){
-	pi[0]=0;int m=len(p),n=len(t);
+// Pasar d el arreglo
+void kmp_vi(string& t, string& s, int *d){
+	pi[0]=0;int m=sz(t),n=sz(s);
 	for(int i=1,k=0;i<m;i++){
-		while(k>0 && p[k]!=p[i])k=pi[k-1];
-		if(p[i]==p[k])k++;
+		while(k>0 && t[k]!=t[i])k=pi[k-1];
+		if(t[i]==t[k])k++;
 		pi[i]=k;
 	}
 	for(int i=0,k=0;i<n;i++){
-		while(k>0 && p[k]!=t[i])k=pi[k-1];
-		if(t[i]==p[k])k++;
+		while(k>0 && t[k]!=s[i])k=pi[k-1];
+		if(s[i]==t[k])k++;
 		d[i]=k;
 		if(k==m)k=pi[k-1];
 	}
--- code/strings.cpp
+++ code/strings.cpp
@@ -1,40 +1,44 @@
-// Funcion para convertir un caracter a un entero
+// Función para convertir un caracter a un entero
 int conv(char ch) { 
-  return ((ch >= 'a' && ch <= 'z') ? ch-'a' : ch-'A'+26);
+    return ((ch>='a' && ch<='z') ? ch-'a' : ch-'A'+26); 
+}
+
+vector<string> split(string& s, char c=' '){
+    vector<string> res;
+    stringstream ss(s);
+    string sub;
+    while(getline(ss, sub, c))res.PB(sub);
+    return res;
 }
 
 string s="abc";
 cout<<s.substr(1)<<"\n";
 cout<<s.substr(0,1)<<"\n";
 
-// El primer parametro es la posicion inicial 
-s.insert(3, "def");
-cout<<s<<"\n";
-// El primer parametro es la posicion y el segundo es la cantidad de caracteres a borrar
-s.erase(3,3);
-cout<<s<<"\n";
-// El primer parametro es la posicion y el segundo es la cantidad de caracteres a reemplazar
-s.replace(0,2,"def");
+s.insert(3, "def"); // El primer parametro es la posicion inicial 
 cout<<s<<"\n";
 
-for(char& c:s){
-  c=toupper(c);
-}
+s.erase(3,3); // El primer parametro es la posicion y el segundo es la cantidad de caracteres a borrar
 cout<<s<<"\n";
 
-for(char& c:s){
-  c=tolower(c);
-}
+s.replace(0,2,"def"); // El primer parametro es la posicion y el segundo es la cantidad de caracteres a reemplazar
 cout<<s<<"\n";
 
-// De string a entero
-s="123";int n;
-istringstream(s)>>n;
-cout<<n<<"\n";
+for(char& c:s)c=toupper(c);
+cout<<s<<"\n";
 
-// De entero a string
+for(char& c:s)c=tolower(c);
+cout<<s<<"\n";
+
+s="123";
+int n=stoi(s); // De string a entero
+
+s="010101101";
+n = stoi(s, nullptr, 2);
+
 n=456;
-ostringstream os;
-os<<n;
-s=os.str();
+s=to_string(n); // De entero a string
 cout<<s<<"\n";
+
+s="123.456";
+double d=stod(s); // De string a double
