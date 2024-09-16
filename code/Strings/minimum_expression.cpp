404d3b9a566571c2e86eec23b8846f102d066fa4
drifterDev <maalvarezmu@unal.edu.co>
2024-09-16 09:49:20 -0500

    Strings

---
+++ code/Strings/minimum_expression.cpp
@@ -0,0 +1,11 @@
+// O(n)
+int minimum_expression(string s){
+	s=s+s;int n=(int)s.size(),i=0,j=1,k=0;
+	while(i+k<n && j+k<n){
+		if(s[i+k]==s[j+k])k++;
+		else if(s[i+k]>s[j+k])i=i+k+1,k=0; // cambiar por < para max
+		else j=j+k+1,k=0;
+		if(i==j)j++;
+	}
+	return min(i, j);
+}
