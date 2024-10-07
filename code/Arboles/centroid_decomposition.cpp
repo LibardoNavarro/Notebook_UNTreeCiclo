55805f389a16b03f19159a56f2e1573e1c70ee3c
drifterDev <maalvarezmu@unal.edu.co>
2024-10-07 11:22:51 -0500

    Árboles

--- code/Arboles/centroid_decomposition.cpp
+++ code/Arboles/centroid_decomposition.cpp
@@ -1,19 +1,16 @@
-// O(nlogn)
-struct CentroidDecomposition{ 
+// O(nlog(n))
+struct CentroidDecomposition{
 	int dad[maxn],sz[maxn];
 	set<int> adj[maxn]; // check, proc
 
 	int operator[](int i){return dad[i];}
 	void addEdge(int x,int y){adj[x].insert(y);adj[y].insert(x);}
-	
+
 	void build(int v=0, int p=-1){
 		int n=dfsSz(v, p); 
 		int centroid=dfsCentroid(v, p, n);
 		dad[centroid]=p;
-		for(int u:adj[centroid]){
-			if(p==u)continue;
-			// count paths
-		}
+		// add dfs for paths
 		for(int u:adj[centroid]){
 			adj[u].erase(centroid);
 			build(u,centroid);
--- code/Arboles/sack.cpp
+++ code/Arboles/sack.cpp
@@ -1,28 +1,20 @@
 const int maxn = 1e5+5;
-int ver[2*maxn],st[maxn],ft[maxn];
-int len[maxn],dep[maxn];
+int st[maxn], ft[maxn], ver[2*maxn];
+int len[maxn], n, q, pos=0;
 vi adj[maxn];
-int n,pos=0;
-
-void prec(int v=0, int p=-1){
-	len[v]=1;ver[pos]=v;
-	st[v]=pos++;
-	for(int u:adj[v]){
-		if(u==p)continue;
-		dep[u]=dep[v]+1;
-		prec(u,v);
-		len[v]+=len[u];
-	}
-	ver[pos]=v;
-	ft[v]=pos++;
-}
 
 bool vis[maxn];
 void ask(int v, bool add){
-	if(vis[v] && !add)// delete node
-	else if(!vis[v] && add)// add node
+	if(vis[v] && !add){
+		vis[v]=false;
+		// delete node
+	}else if(!vis[v] && add){
+		vis[v]=true;
+		// add node
+	}
 }
 
+// O(nlogn)
 void dfs(int v=0, int p=-1, bool keep=true){
 	int mx=0,id=-1;
 	for(int u:adj[v]){
@@ -33,16 +25,18 @@ void dfs(int v=0, int p=-1, bool keep=true){
 		}
 	}
 	for(int u:adj[v]){
-		if(u==p || u==id)continue;
-		dfs(u,v,0); 
+		if(u!=p && u!=id)
+			dfs(u,v,0); 
 	}
 	if(id!=-1)dfs(id,v,1); 
 	for(int u:adj[v]){
 		if(u==p || u==id)continue;
-		for(int p=st[u];p<ft[u];++p)ask(ver[p], 1);
+		for(int p=st[u];p<ft[u];++p)
+			ask(ver[p], 1);
 	}
 	ask(v, 1);
 	// answer queries
 	if(keep)return;
-	for(int p=st[v];p<ft[v];++p)ask(ver[p], 0);
+	for(int p=st[v];p<ft[v];++p)
+		ask(ver[p], 0);
 }
