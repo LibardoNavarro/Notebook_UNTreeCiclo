2231ba9cc78855bbe37f298a3b4e468efa1fba44
Efra_gR <efrain200414@gmail.com>
2024-10-15 12:32:52 -0500

    upd Halfplanes

--- code/Geometria/halfplanes.cpp
+++ code/Geometria/halfplanes.cpp
@@ -75,3 +75,34 @@ vector<pt> hp_intersect(vector<Halfplane>& H) {
     // remove repeated points if needed
     return ret;
 }
+
+
+// ----------------------------------------------------
+
+// intersection of halfplanes
+vector<pt> hp_intersect(vector<halfplane>& b){
+    vector<pt> box = {{inf, inf}, {-inf, inf}, {-inf, -inf}, {inf, -inf}};
+    for(int i = 0; i < 4; i++){
+        b.push_back({box[i], box[(i + 1) % 4]});
+    }
+    sort(b.begin(), b.end());
+    int n = b.size(), q = 1, h = 0;
+    vector<halfplane> c(n + 10);
+    for(int i = 0; i < n; i++){
+        while(q < h && b[i].out(inter(c[h], c[h - 1]))) h--;
+        while(q < h && b[i].out(inter(c[q], c[q + 1]))) q++;
+        c[++h] = b[i];
+        if(q < h && abs(cross(c[h].pq, c[h-1].pq)) < EPS){
+            if(dot(c[h].pq, c[h - 1].pq) <= 0) return {};
+            h--;
+            if(b[i].out(c[h].p)) c[h] = b[i];
+        }
+    }
+    while(q < h - 1 && c[q].out(inter(c[h], c[h - 1]))) h--;
+    while(q < h - 1 && c[h].out(inter(c[q], c[q + 1]))) q++;
+    if(h - q <= 1) return {};
+    c[h + 1] = c[q];
+    vector<pt> s;
+    for(int i = q; i < h + 1; i++) s.pb(inter(c[i], c[i + 1]));
+    return s;
+}
