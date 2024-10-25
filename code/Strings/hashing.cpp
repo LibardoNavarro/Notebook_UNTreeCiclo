1cadb178ca2a8c5cb01440c8afbef6395ad56d1f
drifterDev <maalvarezmu@unal.edu.co>
2024-10-25 11:01:06 -0500

    Hashing

--- code/Strings/hashing.cpp
+++ code/Strings/hashing.cpp
@@ -1,31 +1,31 @@
-1000234999, 1000567999, 1000111997, 1000777121, 1001265673, 1001864327, 999727999, 1070777777
+// 1000234999, 1000567999, 1000111997, 1000777121, 1001265673, 1001864327, 999727999, 1070777777
 const int mod[2] = { 1001864327, 1001265673 }; 
-const ii base(257, 367), zero(0, 0), one(1, 1);
+const ii base(257, 367); // > alpha (primo)
 const int maxn = 1e6;
-
-inline int add(int a, int b, int m){return a+b>=m?a+b-m:a+b;}
-inline int sbt(int a, int b, int m){return a-b<0?a-b+m:a-b;}
-inline int mul(int a, int b, int m){return ll(a)*b%m;} 
-inline ll operator ! (const ii a){return (ll(a.first)<<32)|a.second;}
-inline ii operator + (const ii& a, const ii& b){return {add(a.first, b.first, mod[0]), add(a.second, b.second, mod[1])};}
-inline ii operator - (const ii& a, const ii& b){return {sbt(a.first, b.first, mod[0]), sbt(a.second, b.second, mod[1])};}
-inline ii operator * (const ii& a, const ii& b){return {mul(a.first, b.first, mod[0]), mul(a.second, b.second, mod[1])};}
+int add(int a, int b, int m){return a+b>=m?a+b-m:a+b;}
+int sbt(int a, int b, int m){return a-b<0?a-b+m:a-b;}
+int mul(int a, int b, int m){return ll(a)*b%m;} 
+ll operator ! (const ii a){return (ll(a.first)<<32)|a.second;}
+ii operator + (const ii& a, const ii& b){return {add(a.first, b.first, mod[0]), add(a.second, b.second, mod[1])};}
+ii operator - (const ii& a, const ii& b){return {sbt(a.first, b.first, mod[0]), sbt(a.second, b.second, mod[1])};}
+ii operator * (const ii& a, const ii& b){return {mul(a.first, b.first, mod[0]), mul(a.second, b.second, mod[1])};}
 
 ii p[maxn+1];
 void prepare(){	// Acordate del prepare()!!
-	p[0]=one;
+	p[0]={1,1};
 	for(int i=1;i<=maxn;i++)p[i]=p[i-1]*base;
 }
+
+ii unite(ii a, ii b, int lenb){return a*p[lenb]+b;}
 
 template <class type> 
 struct hashing{
 	vector<ii> h;
+	ii get(int l, int r){return h[r+1]-h[l]*p[r-l+1];}
 	hashing(type& t){
-		h.resize((int)t.size()+1);
-		h[0]=zero;
-		for(int i=1;i<(int)h.size();++i)
+		h.resize(sz(t)+1);
+		h[0]={0,0};
+		for(int i=1;i<sz(h);++i)
 			h[i]=h[i-1]*base + ii{t[i-1], t[i-1]};
 	}
-	ii get(int l, int r){return h[r+1]-h[l]*p[r-l+1];}
 };
-ii combine(ii a, ii b, int lenb){return a*p[lenb]+b;}
