af7ffff171a53f0753c27863744067a104ec76c1
Efra_gR <efrain200414@gmail.com>
2025-03-09 18:51:39 -0500

    add kd_tree

--- code/Geometria/kd_tree.cpp
+++ code/Geometria/kd_tree.cpp
@@ -1,49 +1,94 @@
-// given a set of points, answer queries of nearest point in O(log(n))
-bool onx(pt a, pt b){return a.x < b.x;}
-bool ony(pt a, pt b){return a.y < b.y;}
-struct Node {
-	pt pp;
-	lf x0 = inf, x1 = -inf, y0 = inf, y1 = -inf;
-	Node *first = 0, *second = 0;
+const ll INF = 2e18;
+const int D = 3; // dimension
  
-	ll distance(pt p){
-		ll x = min(max(x0, p.x), x1);
-		ll y = min(max(y0, p.y), y1);
-		return norm2(pt(x, y) - p);
-	}
- 
-	Node(vector<pt>&& vp) : pp(vp[0]){
-		for(pt p : vp){
-			x0 = min(x0, p.x);
-            x1 = max(x1, p.x);
-			y0 = min(y0, p.y); 
-            y1 = max(y1, p.y);
-		}
-		if(vp.size() > 1){
-			sort(all(vp), x1 - x0 >= y1 - y0 ? onx : ony);
-			int m = vp.size() / 2;
-			first = new Node({vp.begin(), vp.begin() + m});
-			second = new Node({vp.begin() + m, vp.end()});
-		}
+struct ptd{
+	int p[D];
+	bool operator !=(const ptd &a) const {
+		bool ok = 1;
+		for(int i = 0; i < D; i++) ok &= (p[i] == a.p[i]);
+		return !ok;
 	}
 };
  
-struct KDTree {
-	Node* root;
-	KDTree(const vector<pt>& vp): root(new Node({all(vp)})){}
-	pair<ll, pt> search(pt p, Node *node){
-		if(!node->first){
-			// avoid query point as answer
-			// if(p.x == node->pp.x && p.y == node->pp.y) return {inf, pt()};
-			return {norm2(p-node->pp), node->pp};
-		}
-		Node *f = node->first, *s = node->second;
-		ll bf = f->distance(p), bs = s -> distance(p);
-		if(bf > bs) swap(bf, bs), swap(f, s);
-		auto best = search(p, f);
-		if(bs < best.ff) best = min(best, search(p, s));
-		return best;
-	}
-	pair<ll, pt> nearest(pt p){ return search(p, root); }
+struct kd_node{
+	ptd p;
+	int axis;
+	kd_node *left, *right;
 };
  
+struct cmp_points {
+	int axis;
+	cmp_points(){}
+	cmp_points(int x): axis(x){}
+	
+	bool operator ()(const ptd &a, const ptd &b) const {
+		return a.p[axis] < b.p[axis];
+	}
+};
+ 
+ll dis2(ptd a, ptd b) {
+	ll ans = 0;
+	for(int i = 0; i < D; i++) ans += (a.p[i] - b.p[i]) * 1ll * (a.p[i] - b.p[i]);
+	return ans;
+}
+ 
+struct KDTree{
+	vector<ptd> arr;
+	kd_node* root;
+ 
+	KDTree(vector<ptd> &vptd): arr(vptd){	
+		build(root, 0, sz(vptd) - 1, 0);
+	}
+    
+    // O(nlogn)
+	void build(kd_node* &node, int l, int r, int axis) {
+		if(l > r) {
+			node = nullptr;
+			return;
+		}
+	  
+		node = new kd_node();
+	  
+		if(l == r) {
+			node->p = arr[l];
+			node->left = nullptr;
+			node->right = nullptr;
+			return;
+		}
+	  
+		int mid = (l + r) / 2;
+	  
+		nth_element(arr.begin() + l, arr.begin() + mid, arr.begin() + r + 1, cmp_points(axis));
+		node->p = arr[mid];
+		node->axis = axis;
+		build(node->left, l, mid, (axis + 1) % D);
+		build(node->right, mid + 1, r, (axis + 1) % D);
+	}
+	
+	void nearest(kd_node* node, ptd q, ll &ans) {
+		if(node == NULL) return;
+	
+		if(node->left == NULL && node->right == NULL) {
+			if(q != node->p) ans = min(ans, dis2(node->p, q)); // CUIDADO
+			return;
+		}
+		
+		int value = node->p.p[node->axis];
+		
+		if(q.p[node->axis] <= value) {
+			nearest(node->left, q, ans);
+			if(q.p[node->axis] + sqrt(ans) >= value) nearest(node->right, q, ans);
+		}
+		else {
+			nearest(node->right, q, ans);
+			if(q.p[node->axis] - sqrt(ans) <= value) nearest(node->left, q, ans);
+		}
+	}
+    
+    // O(logn) Returns the squared distance to the nearest point
+	ll nearest(ptd q){
+		ll ans = INF;
+		nearest(root, q, ans);
+		return ans;
+	}
+};
