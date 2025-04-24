464dcc0cbcc89a6396b929fa6186f0ccb4f60375
drifterDev <maalvarezmu@unal.edu.co>
2025-04-23 22:05:26 -0500

    Add some tests and changes for maths

--- code/Matematicas/mobius.cpp
+++ code/Matematicas/mobius.cpp
@@ -1,3 +1,6 @@
+// 1 if n is 1
+// 0 if n has a squared prime factor
+// (-1)^k if n is a product of k distinct prime factors
 const int N = 1e6+1; 
 int mob[N];
 void mobius() {
@@ -9,3 +12,10 @@ void mobius() {
 		}
 	}
 }
+
+// to count coprime pairs
+// ans=n*(n-1)/2
+// for(int x:a){
+// 		for(int y:divisors[a])cnt[y]++;
+// }
+// ans+=(mobius[v]*cnt[v]*(cnt[v]-1))/2
