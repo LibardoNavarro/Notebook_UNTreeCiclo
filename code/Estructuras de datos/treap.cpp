3a1faa45904f5192db88b450f7b7a3ce0d508ca9
drifterDev <maalvarezmu@unal.edu.co>
2025-04-27 22:53:34 -0500

    Add test for treaps and link cut tree

--- code/Estructuras de datos/treap.cpp
+++ code/Estructuras de datos/treap.cpp
@@ -1,17 +1,22 @@
+// Treap => Binary Search Tree + Binary Heap
+// 1. create a empty root (PTreap root=nullptr;)
+// 2. Append the nodes in asc order
+// PTreap tmp=new Treap(x);
+// root=merge(root, tmp);
+
 typedef long long T;
 typedef unsigned long long u64;
 mt19937_64 rng (chrono::steady_clock::now().time_since_epoch().count());
 
 T null = 0;
 struct Treap{
-	Treap *l,*r,*dad;
-	u64 prior;
-	T sz,val;
+	Treap *l,*r,*dad; // left child, right child
+	u64 prior; // random
+	T sz,val; // size subtree, value
 	Treap(T v){
 		l=r=nullptr;
 		prior=rng();
-		val=v;
-		sz=1;
+		val=v;sz=1;
 	}
 	~Treap(){
 		delete l;
@@ -22,41 +27,48 @@ struct Treap{
 typedef Treap* PTreap;
 T cnt(PTreap x){return (!x?0:x->sz);}
 
-void update(PTreap x){
+// updates node with its children information
+void pull(PTreap x){
 	x->sz=cnt(x->l)+cnt(x->r)+1;
 	if(x->l)x->l->dad=x;
 	if(x->r)x->r->dad=x;
 }
 
-pair<PTreap, PTreap> split(PTreap x, T key){ // f <= key < s
+// O(log(n)) divide the treap in two parts
+// [nodes value <= key], [nodes value > key]
+pair<PTreap, PTreap> split(PTreap x, T key){
 	if(!x)return {nullptr, nullptr};
 	if(x->val>key){
 		auto got=split(x->l, key);
 		x->l=got.second;
-		update(x);
+		pull(x);
 		return {got.first, x};
 	}else{
 		auto got=split(x->r, key);
 		x->r=got.first;
-		update(x);
+		pull(x);
 		return {x, got.second};
 	}
 }
 
+// O(log(n)) merge two treap 
+// if all values in treap x < all values in treap y
 PTreap merge(PTreap x, PTreap y){
 	if(!x)return y;
 	if(!y)return x;
 	if(x->prior<=y->prior){
 		x->r=merge(x->r, y);
-		update(x);
+		pull(x);
 		return x;
 	}else{
 		y->l=merge(x, y->l);
-		update(y);
+		pull(y);
 		return y;
 	}
 }
 
+// O(n*log(n)) 
+// Combine two treap into one
 PTreap combine(PTreap x, PTreap y){
 	if(!x)return y;
 	if(!y)return x;
@@ -67,14 +79,18 @@ PTreap combine(PTreap x, PTreap y){
 	return x;
 }
 
-T kth(PTreap& x, int k){ // indexed 0
+// O(log(n))
+// return kth element - indexed 0
+T kth(PTreap& x, int k){ 
 	if(!x)return null;
 	if(k==cnt(x->l))return x->val;
 	if(k<cnt(x->l))return kth(x->l, k);
 	return kth(x->r, k-cnt(x->l)-1);
 }
 
-pair<int, T> lower_bound(PTreap x, T key){ // index, val
+// O(log(n))
+// return {index, val}
+pair<int, T> lower_bound(PTreap x, T key){ 
 	if(!x)return {0, null};
 	if(x->val<key){
 		auto y=lower_bound(x->r, key);
@@ -86,13 +102,10 @@ pair<int, T> lower_bound(PTreap x, T key){ // index, val
 	return y;
 }
 
+// O(n) print the treap
 void dfs(PTreap x){
 	if(!x)return;
 	dfs(x->l);
 	cout<<x->val<<" ";
 	dfs(x->r);
 }
-
-PTreap root=nullptr;
-PTreap tmp=new Treap(x);
-root=merge(root, tmp);
