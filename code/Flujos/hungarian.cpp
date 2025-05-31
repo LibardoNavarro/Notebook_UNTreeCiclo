a81168d0ab7296a4c7a6a83ffa98eb57d19d8072
Libardo-Navarro <josenavarrop05@gmail.com>
2025-05-31 16:36:32 -0500

    Update hungarian.cpp

--- code/Flujos/hungarian.cpp
+++ code/Flujos/hungarian.cpp
@@ -1,116 +1,32 @@
-const int N = 509;
-/* Complexity: O(n^3) but optimized
-It finds minimum cost maximum matching.
-For finding maximum cost maximum matching
-add -cost and return -matching()
-1-indexed */
-struct Hungarian {
-	long long c[N][N], fx[N], fy[N], d[N];
-	int l[N], r[N], arg[N], trace[N];
-	queue<int> q;
-	int start, finish, n;
-	const long long inf = 1e18;
-	Hungarian() {}
-	Hungarian(int n1, int n2): n(max(n1, n2)) {
-		for (int i = 1; i <= n; ++i) {
-			fy[i] = l[i] = r[i] = 0;
-			for (int j = 1; j <= n; ++j) c[i][j] = inf; // make it 0 for maximum cost matching (not necessarily with max count of matching)
-		}
-	}
-	void add_edge(int u, int v, long long cost) {
-		c[u][v] = min(c[u][v], cost);
-	}
-	inline long long getC(int u, int v) {
-		return c[u][v] - fx[u] - fy[v];
-	}
-	void initBFS() {
-		while (!q.empty()) q.pop();
-		q.push(start);
-		for (int i = 0; i <= n; ++i) trace[i] = 0;
-		for (int v = 1; v <= n; ++v) {
-			d[v] = getC(start, v);
-			arg[v] = start;
-		}
-		finish = 0;
-	}
-	void findAugPath() {
-		while (!q.empty()) {
-			int u = q.front();
-			q.pop();
-			for (int v = 1; v <= n; ++v) if (!trace[v]) {
-					long long w = getC(u, v);
-					if (!w) {
-						trace[v] = u;
-						if (!r[v]) {
-							finish = v;
-							return;
-						}
-						q.push(r[v]);
-					}
-					if (d[v] > w) {
-						d[v] = w;
-						arg[v] = u;
-					}
-				}
-		}
-	}
-	void subX_addY() {
-		long long delta = inf;
-		for (int v = 1; v <= n; ++v) if (trace[v] == 0 && d[v] < delta) {
-				delta = d[v];
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+pair<ll, vl> hungarian(const vector<vl> &a) {
+	if (a.empty()) return {0, {}};
+	int n = sz(a) + 1, m = sz(a[0]) + 1;
+	vl u(n), v(m), p(m), ans(n - 1);
+	rep(i,1,n) {
+		p[0] = i;
+		int j0 = 0; // add "dummy" worker 0
+		vl dist(m,LLONG_MAX), pre(m, -1);
+		vector<bool> done(m + 1);
+		do { // dijkstra
+			done[j0] = true;
+			int i0 = p[j0], j1;ll delta = LLONG_MAX;
+			rep(j,1,m) if (!done[j]) {
+				auto cur = a[i0 - 1][j - 1] - u[i0] - v[j];
+				if (cur < dist[j]) dist[j] = cur, pre[j] = j0;
+				if (dist[j] < delta) delta = dist[j], j1 = j;
 			}
-		// Rotate
-		fx[start] += delta;
-		for (int v = 1; v <= n; ++v) if(trace[v]) {
-				int u = r[v];
-				fy[v] -= delta;
-				fx[u] += delta;
-			} else d[v] -= delta;
-		for (int v = 1; v <= n; ++v) if (!trace[v] && !d[v]) {
-				trace[v] = arg[v];
-				if (!r[v]) {
-					finish = v;
-					return;
-				}
-				q.push(r[v]);
+			rep(j,0,m) {
+				if (done[j]) u[p[j]] += delta, v[j] -= delta;
+				else dist[j] -= delta;
 			}
+			j0 = j1;
+		} while (p[j0]);
+		while (j0) { // update alternating path
+			int j1 = pre[j0];
+			p[j0] = p[j1], j0 = j1;
+		}
 	}
-	void Enlarge() {
-		do {
-			int u = trace[finish];
-			int nxt = l[u];
-			l[u] = finish;
-			r[finish] = u;
-			finish = nxt;
-		} while (finish);
-	}
-	long long maximum_matching() {
-		for (int u = 1; u <= n; ++u) {
-			fx[u] = c[u][1];
-			for (int v = 1; v <= n; ++v) {
-				fx[u] = min(fx[u], c[u][v]);
-			}
-		}
-		for (int v = 1; v <= n; ++v) {
-			fy[v] = c[1][v] - fx[1];
-			for (int u = 1; u <= n; ++u) {
-				fy[v] = min(fy[v], c[u][v] - fx[u]);
-			}
-		}
-		for (int u = 1; u <= n; ++u) {
-			start = u;
-			initBFS();
-			while (!finish) {
-				findAugPath();
-				if (!finish) subX_addY();
-			}
-			Enlarge();
-		}
-		long long ans = 0;
-		for (int i = 1; i <= n; ++i) {
-			if (c[i][l[i]] != inf) ans += c[i][l[i]];
-			else l[i] = 0;
-		}
-		return ans;
-	}
-};
+	rep(j,1,m) if (p[j]) ans[p[j] - 1] = j - 1;
+	return {-v[0], ans}; // min cost
+}
