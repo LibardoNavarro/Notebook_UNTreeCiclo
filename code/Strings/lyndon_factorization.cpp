32fca8ced33d70940e4ec03b13a5f7aa815eff78
Libardo-Navarro <josenavarrop05@gmail.com>
2025-10-12 13:21:19 -0500

    Mateo fíjese que si copie caracteres válidos, esa vaina me estaba dando error

--- code/Strings/lyndon_factorization.cpp
+++ code/Strings/lyndon_factorization.cpp
@@ -1,10 +1,10 @@
 // A string is called simple if it is strictly smaller than all its nontrivial cyclic shifts.
-// The Lyndon factorization of the string  is s = w1 w2 ... wk
-// where all strings  wi are simple, and they are in non-increasing order
+// The Lyndon factorization of the string is s = w1 w2 ... wk
+// where all strings wi are simple, and they are in non-increasing order
 // w1 >= w2 >= ... >= wk
 // this factorization exists and it is unique 
 
-// O(n )
+// O(n)
 vector<string> duval(string& s){
 	vector<string> factorization;
 	int n=sz(s),i=0;
