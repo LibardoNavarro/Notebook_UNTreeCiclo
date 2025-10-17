0f3652e497dbe26269487d19891d67feab1d5057
Libardo-Navarro <josenavarrop05@gmail.com>
2025-10-16 19:42:11 -0500

    gauss jordan

--- code/Matematicas/gauss_jordan.cpp
+++ code/Matematicas/gauss_jordan.cpp
@@ -1,46 +1,32 @@
-// O(min(n, m)*n*m)
-const double EPS = 1e-9;
-const int INF = 2; // it doesn't actually have to be infinity or a big number
-
-int gauss (vector < vector<double> > a, vector<double> & ans) {
-	int n = (int) a.size();
-	int m = (int) a[0].size() - 1;
-
-	vector<int> where (m, -1);
-	for (int col=0, row=0; col<m && row<n; ++col) {
-		int sel = row;
-		for (int i=row; i<n; ++i)
-			if (abs (a[i][col]) > abs (a[sel][col]))
-				sel = i;
-		if (abs (a[sel][col]) < EPS)
-			continue;
-		for (int i=col; i<=m; ++i)
-			swap (a[sel][i], a[row][i]);
-		where[col] = row;
-
-		for (int i=0; i<n; ++i)
-			if (i != row) {
-				double c = a[i][col] / a[row][col];
-				for (int j=col; j<=m; ++j)
-					a[i][j] -= a[row][j] * c;
-			}
-		++row;
-	}
-
-	ans.assign (m, 0);
-	for (int i=0; i<m; ++i)
-		if (where[i] != -1)
-			ans[i] = a[where[i]][m] / a[where[i]][i];
-	for (int i=0; i<n; ++i) {
-		double sum = 0;
-		for (int j=0; j<m; ++j)
-			sum += ans[j] * a[i][j];
-		if (abs (sum - a[i][m]) > EPS)
-			return 0;
-	}
-
-	for (int i=0; i<m; ++i)
-		if (where[i] == -1)
-			return INF;
-	return 1;
+const int EPS = 1;
+int gauss (vector<vector<int>> a, vector<int> &ans) {
+  int n = a.size(), m = a[0].size()-1;
+  vector<int> where(m, -1);
+  for(int col = 0, row = 0; col < m && row < n; ++col) {
+    int sel = row;
+    for(int i = row; i < n; ++i)
+      if(abs(a[i][col]) > abs(a[sel][col])) sel = i;
+    if(abs(a[sel][col]) < EPS) continue;
+    swap(a[sel], a[row]);
+    where[col] = row;
+    for(int i = 0; i < n; ++i)
+      if(i != row) {
+        int c = divide(a[i][col], a[row][col]); /// precalc inverses
+        for(int j = col; j <= m; ++j)
+          a[i][j] = sbt(a[i][j], mul(a[row][j], c));
+      }
+    ++row;
+  }
+  ans.assign(m, 0);
+  for(int i = 0; i < m; ++i)
+    if(where[i] != -1) ans[i] = divide(a[where[i]][m], a[where[i]][i]);
+  for(int i = 0; i < n; ++i) {
+    int sum = 0;
+    for(int j = 0; j < m; ++j)
+      sum = add(sum, mul(ans[j], a[i][j]));
+    if(sum != a[i][m]) return 0;
+  }
+  for(int i = 0; i < m; ++i)
+    if(where[i] == -1) return -1; /// infinite solutions
+  return 1;
 }
